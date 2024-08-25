f68c1bcd973d52bd98010b51376f315d893209a7
Mario <jmario_vz@outlook.com>
2024-08-24 19:47:33 -0700

    finalizada etapa 1 seccion 7

---
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -0,0 +1,23 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { GifsService } from '../../../gifs/services/gifs.service';
+
+@Component({
+  selector: 'shared-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrl: './sidebar.component.css',
+})
+export class SidebarComponent {
+  constructor(private gifService:GifsService) {
+
+  }
+
+  get tags() {
+    return  this.gifService.tagsHistory;
+  }
+
+  searchTag( tag:string) {
+    this.gifService.searchTag(tag);
+  }
+
+ }
