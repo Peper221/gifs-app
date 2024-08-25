610170be3fc8407708c7f3e6b3cec3a0b4d6b5d5
Mario <jmario_vz@outlook.com>
2024-08-25 15:54:21 -0700

    arreglados detalles con sidebar y home-page

--- src/app/gifs/pages/home-page/home-page.component.ts
+++ src/app/gifs/pages/home-page/home-page.component.ts
@@ -5,7 +5,7 @@ import { GifsService } from '../../services/gifs.service';
 @Component({
   selector: 'gifs-home-page',
   templateUrl: './home-page.component.html',
-
+  styleUrl:'./home-page.css'
 })
 export class HomePageComponent {
   constructor(private gifsService:GifsService) {}
